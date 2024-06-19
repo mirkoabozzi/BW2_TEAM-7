24f64699744d6814a0fee568d6f5879b5e61f9ad
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-19 10:37:55 +0200

    fetch album + playlist

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,139 @@
 const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=san_pacho";
 const url2 = "https://deezerdevs-deezer.p.rapidapi.com/search?q=hugel";
 const url3 = "https://deezerdevs-deezer.p.rapidapi.com/search?q=wade";
+const url4 = "https://deezerdevs-deezer.p.rapidapi.com/search?q=paco_osuna";
+const url5 = "https://deezerdevs-deezer.p.rapidapi.com/search?q=low_steppa";
+const url6 = "https://deezerdevs-deezer.p.rapidapi.com/search?q=italobros";
+
+const albumUrl = "https://deezerdevs-deezer.p.rapidapi.com/album/75621062";
+const albumUrl2 = "https://deezerdevs-deezer.p.rapidapi.com/album/299931";
+const albumUrl3 = "https://deezerdevs-deezer.p.rapidapi.com/album/299782827";
+const albumUrl4 = "https://deezerdevs-deezer.p.rapidapi.com/album/10901704";
+const albumUrl5 = "https://deezerdevs-deezer.p.rapidapi.com/album/545453842";
+const albumUrl6 = "https://deezerdevs-deezer.p.rapidapi.com/album/584357862";
+
+const albumArtist = "https://deezerdevs-deezer.p.rapidapi.com/artist/14069";
+const albumArtist2 = "https://deezerdevs-deezer.p.rapidapi.com/artist/14069";
+const albumArtist3 = "https://deezerdevs-deezer.p.rapidapi.com/artist/14069";
+const albumArtist4 = "https://deezerdevs-deezer.p.rapidapi.com/artist/14069";
+const albumArtist5 = "https://deezerdevs-deezer.p.rapidapi.com/artist/14069";
+const albumArtist6 = "https://deezerdevs-deezer.p.rapidapi.com/artist/14069";
 
 const playlistArray = [25, 50, 1220, 2500, 5650, 6500, 3250, 4760, 450, 9850];
 
-const randomPlaylist = Math.floor(Math.random() * playlistArray.length);
+const randomPlaylists = (array) => {
+  const randomNumber = Math.floor(Math.random() * array.length);
 
-const playlistUrl = "https://deezerdevs-deezer.p.rapidapi.com/playlist/" + playlistArray[randomPlaylist];
-
-console.log(randomPlaylist);
+  return "https://deezerdevs-deezer.p.rapidapi.com/playlist/" + array[randomNumber];
+};
 
 const row1 = document.getElementById("row1");
 const row2 = document.getElementById("row2");
 const row3 = document.getElementById("row3");
 const playlistRow = document.getElementById("playlistRow");
+
+fetchAlbum = async (albumUrl, options, row) => {
+  const response = await fetch(albumUrl, options);
+  const album = await response.json();
+  console.log("album", album);
+  // songs = album.tracks.data;
+  const response2 = await fetch(albumUrl2, options);
+  const album2 = await response2.json();
+
+  const response3 = await fetch(albumUrl3, options);
+  const album3 = await response3.json();
+
+  const response4 = await fetch(albumUrl4, options);
+  const album4 = await response4.json();
+
+  const response5 = await fetch(albumUrl5, options);
+  const album5 = await response5.json();
+
+  const response6 = await fetch(albumUrl6, options);
+  const album6 = await response6.json();
+
+  let albums = [album, album2, album3, album4, album5, album6];
+  // console.log("albums", albums);
+
+  for (let index = 0; index < albums.length; index++) {
+    // console.log("index", albums[index]);
+    const col = document.createElement("div");
+    col.classList.add("col-6", "col-md-4", "col-lg-3", "col-xl-2");
+    // if (index === 5) {
+    //   col.classList.add("d-lg-none");
+    // }
+    // if (index === 4) {
+    //   col.classList.add("d-md-none");
+    // }
+
+    if (index === 5) {
+      col.classList.add("hide-on-xl");
+      // console.log("saaaaaas");
+    }
+    if (index === 4) {
+      col.classList.add("hide-on-lg");
+    }
+    if (index === 3) {
+      col.classList.add("hide-on-md");
+    }
+    if (index === 2) {
+      col.classList.add("hide-on-sm");
+    }
+
+    const card = document.createElement("div");
+    card.classList.add("card", "border-0");
+    // card.style.width = "180px";
+
+    const divPic = document.createElement("div");
+    divPic.classList.add("position-relative");
+
+    const pic = document.createElement("img");
+    pic.classList.add("bd-placeholder-img", "card-img-top", "p-2");
+    pic.style.cursor = "pointer";
+    pic.style.objectFit = "contain";
+
+    pic.setAttribute("src", albums[index].cover);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const artist = document.createElement("h5");
+    artist.classList.add("card-title", "text-truncate");
+    artist.style.cursor = "pointer";
+    artist.innerText = albums[index].artist.name;
+
+    const title = document.createElement("p");
+    title.classList.add("card-text", "text-truncate");
+    title.innerText = albums[index].title;
+    title.style.cursor = "pointer";
+
+    const playBtn = document.createElement("button");
+    playBtn.innerHTML = `<svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16" width="20px" heigth="20px" class="Svg-sc-ytk21e-0 dYnaPI"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>`;
+    playBtn.classList.add("btn", "rounded-circle", "bg-success", "d-flex", "justify-content-center", "align-items-center", "position-absolute", "bottom-0", "end-0", "d-none", "p-2");
+
+    col.addEventListener("mouseover", (e) => {
+      playBtn.classList.remove("d-none");
+      card.classList.add("shadow-lg");
+    });
+    col.addEventListener("mouseout", (e) => {
+      playBtn.classList.add("d-none");
+      card.classList.remove("shadow-lg");
+    });
+
+    divPic.appendChild(playBtn);
+
+    cardBody.appendChild(artist);
+    cardBody.appendChild(title);
+
+    divPic.appendChild(pic);
+    card.appendChild(divPic);
+    card.appendChild(cardBody);
+
+    col.appendChild(card);
+
+    row.appendChild(col);
+  }
+};
 
 fetchSong = async (url, options, row) => {
   const response = await fetch(url, options);
@@ -48,13 +168,26 @@ fetchSong = async (url, options, row) => {
     const song = songs[randomSong];
 
     const col = document.createElement("div");
-    col.classList.add("col");
+    col.classList.add("col-6", "col-md-4", "col-lg-3", "col-xl-2");
     // if (index === 5) {
     //   col.classList.add("d-lg-none");
     // }
     // if (index === 4) {
     //   col.classList.add("d-md-none");
     // }
+    if (index === 5) {
+      col.classList.add("hide-on-xl");
+      // console.log("saaaaaas");
+    }
+    if (index === 4) {
+      col.classList.add("hide-on-lg");
+    }
+    if (index === 3) {
+      col.classList.add("hide-on-md");
+    }
+    if (index === 2) {
+      col.classList.add("hide-on-sm");
+    }
 
     const card = document.createElement("div");
     card.classList.add("card", "border-0");
@@ -64,7 +197,7 @@ fetchSong = async (url, options, row) => {
     divPic.classList.add("position-relative");
 
     const pic = document.createElement("img");
-    pic.classList.add("bd-placeholder-img", "img-fluid", "p-2");
+    pic.classList.add("bd-placeholder-img", "card-img-top", "p-2");
     pic.style.cursor = "pointer";
     pic.style.objectFit = "contain";
 
@@ -113,13 +246,33 @@ fetchSong = async (url, options, row) => {
   songIndex = [];
 };
 
-const fetchPlaylist = async (playlistUrl, options) => {
+fetchPlaylist = async (options) => {
   try {
-    const response = await fetch(playlistUrl, options);
+    const response = await fetch(randomPlaylists(playlistArray), options);
     const playlist = await response.json();
-    console.log(playlist);
+    console.log("playlist", playlist);
+    // console.log("tracks", playlist.tracks);
+    // console.log("data", playlist.tracks.data);
 
-    playlistArray.forEach((element) => {
+    const response2 = await fetch(randomPlaylists(playlistArray), options);
+    const playlist2 = await response2.json();
+
+    const response3 = await fetch(randomPlaylists(playlistArray), options);
+    const playlist3 = await response3.json();
+
+    const response4 = await fetch(randomPlaylists(playlistArray), options);
+    const playlist4 = await response4.json();
+
+    const response5 = await fetch(randomPlaylists(playlistArray), options);
+    const playlist5 = await response5.json();
+
+    const response6 = await fetch(randomPlaylists(playlistArray), options);
+    const playlist6 = await response6.json();
+
+    const arrayPlaylists = [playlist, playlist2, playlist3, playlist4, playlist5, playlist6];
+
+    for (let index = 0; index < arrayPlaylists.length; index++) {
+      // console.log("playlist index", arrayPlaylists[index]);
       const col = document.createElement("div");
       col.classList.add("col-6", "col-lg-4", "my-1");
 
@@ -130,13 +283,13 @@ const fetchPlaylist = async (playlistUrl, options) => {
       // picContainer.classList.add("position-relative");
 
       const pic = document.createElement("img");
-      pic.setAttribute("src", playlist.picture_small);
+      pic.setAttribute("src", arrayPlaylists[index].picture_small);
 
       const titleContainer = document.createElement("div");
       titleContainer.classList.add("ms-2", "me-auto");
 
       const title = document.createElement("p");
-      title.innerText = playlist.title;
+      title.innerText = arrayPlaylists[index].title;
 
       const playBtn = document.createElement("button");
       playBtn.innerHTML = `<svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16" width="20px" heigth="20px" class="Svg-sc-ytk21e-0 dYnaPI"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>`;
@@ -163,14 +316,112 @@ const fetchPlaylist = async (playlistUrl, options) => {
       col.appendChild(divFlex);
 
       playlistRow.appendChild(col);
-    });
+    }
   } catch (error) {
     console.error(error);
   }
 };
 
-const resizeCheck = () => {
-  if (screen.width) {
+fetchAlbum = async (albumUrl, options, row) => {
+  const response = await fetch(albumUrl, options);
+  const album = await response.json();
+  console.log("album", album);
+  // songs = album.tracks.data;
+  const response2 = await fetch(albumUrl2, options);
+  const album2 = await response2.json();
+
+  const response3 = await fetch(albumUrl3, options);
+  const album3 = await response3.json();
+
+  const response4 = await fetch(albumUrl4, options);
+  const album4 = await response4.json();
+
+  const response5 = await fetch(albumUrl5, options);
+  const album5 = await response5.json();
+
+  const response6 = await fetch(albumUrl6, options);
+  const album6 = await response6.json();
+
+  let albums = [album, album2, album3, album4, album5, album6];
+  // console.log("albums", albums);
+
+  for (let index = 0; index < albums.length; index++) {
+    // console.log("index", albums[index]);
+    const col = document.createElement("div");
+    col.classList.add("col-6", "col-md-4", "col-lg-3", "col-xl-2");
+    // if (index === 5) {
+    //   col.classList.add("d-lg-none");
+    // }
+    // if (index === 4) {
+    //   col.classList.add("d-md-none");
+    // }
+
+    if (index === 5) {
+      col.classList.add("hide-on-xl");
+      // console.log("saaaaaas");
+    }
+    if (index === 4) {
+      col.classList.add("hide-on-lg");
+    }
+    if (index === 3) {
+      col.classList.add("hide-on-md");
+    }
+    if (index === 2) {
+      col.classList.add("hide-on-sm");
+    }
+
+    const card = document.createElement("div");
+    card.classList.add("card", "border-0");
+    // card.style.width = "180px";
+
+    const divPic = document.createElement("div");
+    divPic.classList.add("position-relative");
+
+    const pic = document.createElement("img");
+    pic.classList.add("bd-placeholder-img", "card-img-top", "p-2");
+    pic.style.cursor = "pointer";
+    pic.style.objectFit = "contain";
+
+    pic.setAttribute("src", albums[index].cover);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const artist = document.createElement("h5");
+    artist.classList.add("card-title", "text-truncate");
+    artist.style.cursor = "pointer";
+    artist.innerText = albums[index].artist.name;
+
+    const title = document.createElement("p");
+    title.classList.add("card-text", "text-truncate");
+    title.innerText = albums[index].title;
+    title.style.cursor = "pointer";
+
+    const playBtn = document.createElement("button");
+    playBtn.innerHTML = `<svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16" width="20px" heigth="20px" class="Svg-sc-ytk21e-0 dYnaPI"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>`;
+    playBtn.classList.add("btn", "rounded-circle", "bg-success", "d-flex", "justify-content-center", "align-items-center", "position-absolute", "bottom-0", "end-0", "d-none", "p-2");
+
+    col.addEventListener("mouseover", (e) => {
+      playBtn.classList.remove("d-none");
+      card.classList.add("shadow-lg");
+    });
+    col.addEventListener("mouseout", (e) => {
+      playBtn.classList.add("d-none");
+      card.classList.remove("shadow-lg");
+    });
+
+    divPic.appendChild(playBtn);
+
+    cardBody.appendChild(artist);
+    cardBody.appendChild(title);
+
+    divPic.appendChild(pic);
+    card.appendChild(divPic);
+    card.appendChild(cardBody);
+
+    col.appendChild(card);
+
+    row.appendChild(col);
   }
 };
 
@@ -184,10 +435,11 @@ window.addEventListener("DOMContentLoaded", async () => {
   };
 
   try {
-    fetchSong(url, options, row1);
+    fetchAlbum(albumUrl, options, row1);
+    // fetchSong(url, options, row1);
     fetchSong(url2, options, row2);
     fetchSong(url3, options, row3);
-    fetchPlaylist(playlistUrl, options);
+    fetchPlaylist(options);
   } catch (error) {
     console.error(error);
   }
